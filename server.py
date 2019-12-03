7dce7fda64bc58b3f559a666c9e2dfda8e7b9bba
Zoe Kahana <zkahana@princeton.edu>
2019-12-03 00:28:55 -0500

    Update server.py

--- server.py
+++ server.py
@@ -61,9 +61,10 @@ def index():
     # Render the home page, passing in the list of plants.
     html = render_template('index.html', 
     plants = plants,
-    all_species = all_species, 
+    all_species = all_species,
     status_vals = status_vals,
     dec_or_evg_vals = dec_or_evg_vals)
+    
     response = make_response(html)
 
     return response
