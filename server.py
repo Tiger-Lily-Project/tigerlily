3f93d48796fa1a303e841cc49e5395f50d559034
Wendy Ho <wendyho91098@gmail.com>
2019-12-04 20:57:01 -0500

    ok hopefully this works

--- server.py
+++ server.py
@@ -184,6 +184,17 @@ def about():
 
     return response
 #-----------------------------------------------------------------------
+# Renders the "about us" page.
+@app.route('/')
+@app.route('/test')
+def test():
+
+    # Render the catalog page, passing in the list of species.
+    html = render_template('test.html')
+    response = make_response(html)
+
+    return response
+#-----------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10101, debug=True)
